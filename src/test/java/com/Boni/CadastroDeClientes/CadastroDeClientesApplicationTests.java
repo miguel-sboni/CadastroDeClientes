0b9f6df0603195dc85382e8855b37f1e3763fab9
Miguel Boni <migueldilvalive@gmail.com>
2026-04-25 16:29:04 -0300

    primeiro commit

---
+++ src/test/java/com/Boni/CadastroDeClientes/CadastroDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Boni.CadastroDeClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
